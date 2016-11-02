27164c707ebece3c69e98075ca3a8d789e9472b3
unbornchikken <gabor.mezo@outlook.com>
2016-11-02 15:26:09 +0100

    ref-union tests in progress

--- test/suites/refTypes.js
+++ test/suites/refTypes.js
@@ -8,8 +8,9 @@ const ref = fastcall.ref;
 const Promise = require('bluebird');
 const async = Promise.coroutine;
 const StructType = fastcall.StructType;
+const UnionType = fastcall.UnionType;
 
-describe.only('Ref Types', function () {
+describe('Ref Types', function () {
     let libPath = null;
     let lib = null;
     before(async(function* () {
@@ -187,5 +188,151 @@ describe.only('Ref Types', function () {
 
             assert.equal(result, 1 * 2 * 3);
         });
+    });
+
+    describe.only('Union', function () {
+        it('could be created by plain object definition', function () {
+            const result = lib.union({
+                TUnion: {
+                    a: 'short',
+                    b: 'int64',
+                    c: ref.types.long
+                }
+            });
+
+            assert.equal(result, lib);
+            testUnionInterface();            
+        });
+
+        it('could be created from UnionType', function () {
+            const TUnion = new UnionType({
+                a: 'short',
+                b: 'int64',
+                c: ref.types.long
+            });
+            const result = lib.union({ TNumbers });
+
+            assert.equal(result, lib);
+            testUnionInterface();            
+        });
+
+        it('should supports C union like syntax', function () {
+            const result = lib.union('union TUnion { short a; int64 b; long c; }');
+
+            assert.equal(result, lib);
+            testUnionInterface();            
+        });
+
+        describe('sync', function () {
+           it('should get referenced by string syntax', function () {
+                lib.struct({
+                    TUnion: {
+                        a: 'short',
+                        b: 'int64',
+                        c: ref.types.long
+                    }
+                })
+                .function('int64 getAFromUnion(TUnion* union)')
+                .function('int64 getBFromUnion(TUnion* union)')
+                .function('int64 getCFromUnion(TUnion* union)');
+
+                testAccessUnionMembersSync();
+            });
+
+            it('should get referenced by node-ffi-like syntax', function () {
+                const TUnion = new UnionType({
+                    a: 'short',
+                    b: 'int64',
+                    c: ref.types.long
+                });
+                lib.function({ getAFromUnion: ['int64', [ ref.refType(TUnion) ]] })
+                .function({ getBFromUnion: ['int64', [ ref.refType(TUnion) ]] })
+                .function({ getCFromUnion: ['int64', [ ref.refType(TUnion) ]] });
+
+                testAccessUnionMembersSync(true);
+            });
+        });
+
+        describe('async', function () {
+           it('should get referenced by string syntax', async(function* () {
+                lib.struct({
+                    TUnion: {
+                        a: 'short',
+                        b: 'int64',
+                        c: ref.types.long
+                    }
+                })
+                .function('int64 getAFromUnion(TUnion* union)')
+                .function('int64 getBFromUnion(TUnion* union)')
+                .function('int64 getCFromUnion(TUnion* union)');
+
+                yield testAccessUnionMembersAsync();
+            }));
+
+            it('should get referenced by node-ffi-like syntax', async(function* () {
+                const TUnion = new UnionType({
+                    a: 'short',
+                    b: 'int64',
+                    c: ref.types.long
+                });
+                lib.function({ getAFromUnion: ['int64', [ ref.refType(TUnion) ]] })
+                .function({ getBFromUnion: ['int64', [ ref.refType(TUnion) ]] })
+                .function({ getCFromUnion: ['int64', [ ref.refType(TUnion) ]] });
+
+                yield testAccessUnionMembersSync(true);
+            }));
+        });
+
+        function testUnionInterface() {
+            assert(_.isObject(lib.unions.TUnion));
+            assert(_.isFunction(lib.interface.TUnion));
+            assert(_.isFunction(lib.unions.TUnion.type));
+            assert.equal(lib.interface.TUnion.union, lib.unions.TUnion);
+            assert.equal(lib.interface.TUnion.type, lib.unions.TUnion.type);
+            
+            const ptr1 = lib.interface.TUnion({
+                a: 1
+            });
+            assert(ptr1 instanceof Buffer);
+            assert.equal(ptr1.type, lib.unions.TUnion.type);
+            assert.equal(ptr1.union, lib.unions.TUnion);
+
+            const union = lib.unions.TUnion.type(ptr1);
+            assert.equal(union.a, 1);
+        }
+
+        function testAccessUnionMembersSync(noname) {
+            assert(_.isFunction(lib.interface.getAFromUnion));
+            assert(_.isFunction(lib.interface.getBFromUnion));
+            assert(_.isFunction(lib.interface.getCFromUnion));
+            
+        //     let result;
+        //     if (!noname) {
+        //         assert(_.isFunction(lib.interface.TNumbers));
+        //         assert.strictEqual(ref.derefType(lib.functions.mulStructMembers.args[0].type), lib.structs.TNumbers.type);
+        //         const ptr = lib.interface.TNumbers({
+        //             a: 1,
+        //             b: 2,
+        //             c: 3
+        //         });
+        //         result = lib.interface.mulStructMembers(ptr);
+        //     }
+        //     else {
+        //         const TNumbers = new StructType({
+        //             a: 'short',
+        //             b: 'int64',
+        //             c: ref.types.long
+        //         });
+
+        //         const numbers = new TNumbers();
+        //         numbers.a = 1;
+        //         numbers.b = 2;
+        //         numbers.c = 3;
+
+        //         result = lib.interface.mulStructMembers(numbers.ref());
+        //     }
+
+        //     assert.equal(result, 1 * 2 * 3);
+        // }
     });
 });
