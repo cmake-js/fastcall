111a62f4418d652af89d0cda81aedd7bbfbf7ec5
unbornchikken <gabor.mezo@outlook.com>
2016-11-11 22:49:26 +0100

    benchmark tunning

--- benchmarks/fastcallRun.js
+++ benchmarks/fastcallRun.js
@@ -26,7 +26,7 @@ function syncRun(lib) {
 
 var asyncRun = async(function* (lib) {
     let result = 0;
-    const addNumbersAsync = lib.functions.addNumbersExp.async();
+    const addNumbersAsync = lib.interface.addNumbersExp.async;
     yield common.measureAsync('addNumbers', 3, async(function* () {
         result = yield addNumbersAsync(yield addNumbersAsync(5.5, 5), yield addNumbersAsync(1.1, 1));
     }));
